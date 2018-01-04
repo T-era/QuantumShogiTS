42396a89b0f0f1f58a204751c4c28f990c7c7e96
t-era <t-era@ray.ocn.ne.jp>
2018-01-04 23:41:19 +0900

    modify build script for PlateEditorTS

--- dbuild.sh
+++ dbuild.sh
@@ -2,6 +2,11 @@ SRC_DIR=./src
 LIB_DIR=${SRC_DIR}/lib
 TARGETS=("common" "rule" "control")
 
+cd vendor/PlateEditorTS/src
+./dbuild.sh
+./build.sh
+cd -
+
 # Compile TypeScript code.
 for fld in ${TARGETS[@]}; do
   tsc -d --out $LIB_DIR/$fld.d.ts $SRC_DIR/ts/$fld/main.ts
